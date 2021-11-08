3b60937941629d2c453573873517d39249b669bb
Joshua <jpdanielskemp@gmail.com>
2021-11-08 21:56:58 +0000

    Getting things set up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'tunes4tuesday',
+    appId: '1:265870369823:web:c3f08ca7dee9d2ab88a12a',
+    storageBucket: 'tunes4tuesday.appspot.com',
+    apiKey: 'AIzaSyD_1Rp7MmAm6RgOaSkvgMEg7GIISZKFjmA',
+    authDomain: 'tunes4tuesday.firebaseapp.com',
+    messagingSenderId: '265870369823',
+    measurementId: 'G-1TJX3V4H03',
+  },
   production: true
 };
